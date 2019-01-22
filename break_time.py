1d059df06408b5286f4ff57cea3c0fe5f00ad627
Jason Ritchie <7eb94c5c@opayq.com>
2019-01-22 10:41:46 -0500

    Added user inputs for number of breaks, gap in between, and YouTube video ID

--- break_time.py
+++ break_time.py
@@ -4,13 +4,22 @@ import time
 # Import webbrowser as we will be opening the client browser and sending to a webpage.
 import webbrowser
 
-# In RUN, display when the date/time the program was started.
-print("You started this program at " + time.ctime())
+
+breaks = input("How many breaks do you want to take?")
+break_gap = input("How many minutes between the breaks?")
+print("\nOnly add what comes after the '?v=' in the YouTube address (https://www.youtube.com/watch?v=hqCsg4U3iqQ).")
+youtube_video_id = input("ID of the YouTube video to show (press enter for default):")
 
 # Function to loop four times, a message and open a website every 2 hours.
-def set_breaktime(hours, minutes, seconds, breaks):
+def set_breaktime(hours, minutes, seconds, breaks, videoId):
     # Define loop_count and start it at 1.
     loop_count = 1
+    # Check the video id
+    if youtube_video_id == '':
+        videoUrl = 'https://www.youtube.com/watch?v=hqCsg4U3iqQ'
+    else:
+        videoUrl = ('https://www.youtube.com/watch?v=%s' % videoId)
+    print('\n\nYou\'ve asked for {} breaks in {} minute intervals.\n\nAt each break the following YouTube video will play:\n    {}'.format(breaks, minutes, videoUrl))
     # Process the contents of the loop the given number of breaks.
     while loop_count <= breaks:
         # Calculate the number of seconds the loop will pause based on user input.
@@ -20,9 +29,11 @@ def set_breaktime(hours, minutes, seconds, breaks):
         # Print the generated message using the current loop_count and the current date/time.
         print(print_message.format(loop_count, time.ctime()))
         # Open the user's web browser and send it to the desired URI.
-        webbrowser.open("https://www.youtube.com/watch?v=hqCsg4U3iqQ")
+        webbrowser.open(videoUrl)
         # Increment the loop by 1.
         loop_count += 1
 
+# In RUN, display when the date/time the program was started.
+print("You started this program at " + time.ctime())
 # Run the set_breaktime function and input (Hours, Minutes, Seconds, Number of times to loop)
-set_breaktime(1, 0, 0, 6)
+set_breaktime(0, int(break_gap), 0, int(breaks), youtube_video_id)
